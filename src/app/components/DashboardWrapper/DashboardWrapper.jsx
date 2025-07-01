207cf63bbf5c2edf53ff15390b7f4cc06074f74a
Ian Lockwood <mrianlockwood@gmail.com>
2025-07-01 11:46:32 -0400

    Add Dashboard page and route

---
+++ src/app/components/DashboardWrapper/DashboardWrapper.jsx
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+  return (
+    <div>
+        <h1 className="text-3xl font-bold text-center my-8">Dashboard</h1>
+    </div>
+  );
+}
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import DashboardWrapper from 'components/DashboardWrapper/DashboardWrapper';
+
+export default function Dashboard() {
+  return (
+    <div>
+        <main>
+            <DashboardWrapper />
+        </main>
+    </div>
+  );
+}
