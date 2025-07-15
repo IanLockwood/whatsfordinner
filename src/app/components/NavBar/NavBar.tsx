5ce9f179a87ed2d3ecdee6c8b401398458340f16
Ian Lockwood <mrianlockwood@gmail.com>
2025-07-15 10:41:37 -0400

    Fix NavBar

--- src/app/components/NavBar/NavBar.tsx
+++ src/app/components/NavBar/NavBar.tsx
@@ -9,20 +9,20 @@ const NAV_ITEMS = [
 
 export default function NavBar() {
     return (
-        <nav className="bg-transparent border-b border-gray-300 mb-[-58]">
+        <nav className="absolute top-0 left-0 w-full bg-transparent border-b border-gray-300">
             <ul className="flex gap-6 list-none m-0 p-0 justify-center">
             {NAV_ITEMS.map(({ name, path }) => (
-                <li
-                    key={path}
-                    className="navbar-item p-4 transition-colors duration-200 hover:bg-gray-200 group"
+            <li
+                key={path}
+                className="navbar-item p-4 transition-colors duration-200 hover:bg-gray-200 group"
+            >
+                <Link
+                href={path}
+                className="no-underline text-gray-800 font-bold italic px-3 py-1 rounded group-hover:text-blue-600 transition-colors duration-200"
                 >
-                    <Link
-                        href={path}
-                        className="no-underline text-gray-800 font-bold italic px-3 py-1 rounded group-hover:text-blue-600 transition-colors duration-200"
-                    >
-                        {name}
-                    </Link>
-                </li>
+                {name}
+                </Link>
+            </li>
             ))}
             </ul>
         </nav>
