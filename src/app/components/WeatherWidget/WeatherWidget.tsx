414822ee753ed2365018e7fe4c3dd1c9bdc9416a
Ian Lockwood <mrianlockwood@gmail.com>
2025-07-15 12:30:17 -0400

    Add row for forecast

--- src/app/components/WeatherWidget/WeatherWidget.tsx
+++ src/app/components/WeatherWidget/WeatherWidget.tsx
@@ -63,45 +63,66 @@ export default function WeatherWidget() {
         return <div>Loading...</div>;
     }
     return (
-        <div className="w-full max-w-[1200px] rounded-lg shadow-md p-6 bg-white text-center">
-            <h3 className="text-center">Weather Data for {weatherData.location.name}, {weatherData.location.region}</h3>
+        <div className="w-full max-w-[1200px]">
 
-            <div className="flex justify-center mb-[-24]">
-                <img
-                    src={weatherData.current?.condition?.icon}
-                    alt={weatherData.current?.condition?.text}
-                    className="h-40 w-40"
+            <div className="w-full max-w-[1200px] rounded-lg shadow-md p-6 bg-white text-center">
+                <h3 className="text-center">Weather Data for {weatherData.location.name}, {weatherData.location.region}</h3>
+
+                <div className="flex justify-center mb-[-24]">
+                    <img
+                        src={weatherData.current?.condition?.icon}
+                        alt={weatherData.current?.condition?.text}
+                        className="h-40 w-40"
                     />
+                </div>
+                
+
+                <h3>{weatherData.current?.temp_f} °F</h3>
+                <p>{weatherData.current?.condition?.text}</p>
+                <p>Feels Like {weatherData.current?.feelslike_f} °C</p>
+                <p> Chance of rain: {weatherData.forecast?.forecastday?.[0]?.day?.daily_chance_of_rain ?? "N/A"} % </p>
+                
+                <br />
+                
+
+
+                <div className="grid grid-cols-2 md:grid-cols-4 gap-4 mt-4">
+                    <div>
+                        <p className="font-semibold">Precipitation</p>
+                        <p>{weatherData.current?.precip_in} in</p>
+                    </div>
+                    <div>
+                        <p className="font-semibold">Humidity</p>
+                        <p>{weatherData.current?.humidity} %</p>
+                    </div>
+                    <div>
+                        <p className="font-semibold">Wind</p>
+                        <p>{weatherData.current?.wind_kph} kph {weatherData.current?.wind_dir}</p>
+                    </div>
+                    <div>
+                        <p className="font-semibold">UV Index</p>
+                        <p>{weatherData.current?.uv}</p>
+                    </div>
+                </div>
             </div>
-            
 
-            <h3>{weatherData.current?.temp_f} °F</h3>
-            <p>{weatherData.current?.condition?.text}</p>
-            <p>Feels Like {weatherData.current?.feelslike_f} °C</p>
-            <p> Chance of rain: {weatherData.forecast?.forecastday?.[0]?.day?.daily_chance_of_rain ?? "N/A"} % </p>
-            
-            <br />
-            
-
-
-            <div className="grid grid-cols-2 md:grid-cols-4 gap-4 mt-4">
-                <div>
-                    <p className="font-semibold">Precipitation</p>
-                    <p>{weatherData.current?.precip_in} in</p>
-                </div>
-                <div>
-                    <p className="font-semibold">Humidity</p>
-                    <p>{weatherData.current?.humidity} %</p>
-                </div>
-                <div>
-                    <p className="font-semibold">Wind</p>
-                    <p>{weatherData.current?.wind_kph} kph {weatherData.current?.wind_dir}</p>
-                </div>
-                <div>
-                    <p className="font-semibold">UV Index</p>
-                    <p>{weatherData.current?.uv}</p>
+            <div className="w-full max-w-[1200px] rounded-lg shadow-md p-6 bg-white mt-4">
+                <div className="flex justify-between gap-4">
+                    <div className="flex-1">Today</div>
+                    <div className="flex-1">201º/12º</div>
+                    <div className="flex-2 flex gap-2">
+                        <img
+                            src={weatherData.current?.condition?.icon}
+                            alt={weatherData.current?.condition?.text}
+                            className="h-8 w-8"
+                        />
+                        <div>Scattered Thunderstorms</div>
+                    </div>
+                    <div className="flex-1">140%</div>
+                    <div className="flex-1">Humidity 99%</div>
                 </div>
             </div>
+
         </div>
     );
 }
